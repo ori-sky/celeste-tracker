efa71b48c804666dbb71977d5ce106e88db5dc11
David Farrell <shokku.ra@gmail.com>
2019-12-04 13:00:20 +0000

    Remove old unused code

--- js/main.js
+++ js/main.js
@@ -10,19 +10,3 @@ window.onload = function() {
 		}
 	}
 }
-
-/*
-window.onload = function() {
-	var tags = document.getElementsByTagName('div')
-	for(var i = 0; i < tags.length; ++i) {
-		var tag = tags[i]
-		if(tag.classList.contains('active')) {
-			tag.classList.remove('active')
-			tag.classList.add('inactive')
-		} else if(tag.classList.contains('inactive')) {
-			tag.classList.remove('inactive')
-			tag.classList.add('active')
-		}
-	}
-}
-*/
